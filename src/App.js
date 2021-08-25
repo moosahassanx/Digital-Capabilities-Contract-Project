2327e75befe334a4d58aa6fa4dcd49ac70879c8a
Moosa <moosa7021@gmail.com>
2021-08-25 16:30:29 +1000

    adding radar graph

--- src/App.js
+++ src/App.js
@@ -1,18 +1,113 @@
 import './App.css';
 import uonImage from './img/uon-logo.png';
 import AssessmentInput from './components/AssessmentInput';
+import Chart from './components/Chart';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Digital Capabilities Survey</h1>
-      <h2>Author: Moosa Hassan</h2>
-      <p>This web page was created with React and GitHub.</p>
-      <img src={uonImage} alt="placeholder-image" />
+class App extends Component {
 
-      <AssessmentInput />
-    </div>
-  );
+  constructor(){
+    super();
+    this.state = {
+      // current data - to be used when the user completes the survey
+      chartData:{},
+
+      // previous data - to be updates when the user uploads csv file as comparison
+      previousChartData: {
+        label:'Previous Score',
+        data:[
+          42,
+          35,
+          22,
+          31,
+          15,
+          35
+        ],
+        backgroundColor: 'rgba(0, 255, 255, 0.1)',
+        borderColor: '#00FFFF',
+        borderWidth: 2
+      }
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: [
+          'Proficiency and Productivity',
+          'Digital Learning and Development',
+          'Digital Creation, Problem Solving and Innocation',
+          'Digital Communication, Collaboration and Participation',
+          'Information Literacy, Data Literacy and Media Literacy',
+          'Digital Identity and Wellbeing'
+        ],
+        datasets:[
+
+          // dataset 1
+          {
+            label:'Current Score',
+            data:[
+              40,
+              29,
+              13,
+              42,
+              36,
+              30
+            ],
+            backgroundColor: 'rgba(00, 255, 00, 0.1)',
+            borderColor: '#00FF00',
+            borderWidth: 2
+          },
+
+          // dataset 2
+          this.state.previousChartData
+        ]
+      }
+    });
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <div className='navbar'>
+          <ul>
+            <li><a class="active" href="#home">Digital Capability Tool</a></li>
+            <li><a href="#news">Code</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#about">Privacy Policy</a></li>
+          </ul>
+        </div>
+
+        <h1 class='firstHeader'>Digital Capabilities Survey</h1>
+        <img className='uon-image' src={uonImage} alt="temporary-placeholder" />
+  
+        <hr></hr>
+        <AssessmentInput />
+        <hr></hr>
+
+        <div className='chart-wrapper'>
+          <h2>Results</h2>
+          <h4>You are a <span className='user-type'>[user_type]</span> user.</h4>
+          <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
+        </div>
+
+        <hr></hr>
+
+        <div>
+          <h2>Compare current results to previous results</h2>
+          <h3>Upload csv file</h3>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/AssessmentInput.js
+++ src/components/AssessmentInput.js
@@ -1,20 +1,124 @@
-import { getElementError } from '@testing-library/react';
+import { useState } from 'react';
 import React from 'react';
 
 const AssessmentInput = () => {
 
-    function  testFunction ()
+    // const [state, function to update the state]
+    const [page0, setPage0] = useState(true);
+    const [page1, setPage1] = useState(false);
+    const [page2, setPage2] = useState(false);
+    
+    const [question1, setQuestion1] = useState("null");
+    const [question2, setQuestion2] = useState("null");
+
+    function nextPageFrom0()
     {
-        alert("calling is working " + document.getElementById("response").value);
+        setQuestion1(document.getElementById("question1").value);
+
+        setPage0(false);
+        setPage1(true);
+        setPage2(false);
+    }
+
+    function nextPageFrom1()
+    {
+        setQuestion2(document.getElementById("question2").value);
+
+        setPage0(false);
+        setPage1(false);
+        setPage2(true);
+    }
+
+    function prevPage()
+    {
+        if(page0 === true)
+        {
+            setPage0(false);
+            setPage1(false);
+            setPage2(false);
+        }
+
+        if(page1 === true)
+        {
+            setPage0(true);
+            setPage1(false);
+            setPage2(false);
+        }
+
+        if(page2 === true)
+        {
+            setPage0(false);
+            setPage1(true);
+            setPage2(false);
+        }
+    }
+
+    function calculate()
+    {
+        // algorithm for calculating user type
+        var totalScore = parseInt(question1) + parseInt(question2);
+
+        if(totalScore < 3)
+        {
+            alert("you are a basic user");
+            return;
+        }
+
+        if(totalScore < 6)
+        {
+            alert("you are an independent user");
+            return;
+        }
+
+        else
+        {
+            alert("you are a proficient user");
+            return;
+        }
     }
 
     return (
         <div className="input-form">
-            <form>
-                <h2>This is an initialised React component</h2>
-                <input id="response"></input>
-                <button className='form-button' type='button' onClick={testFunction}>test</button>
-            </form>
+            {
+                page0 === true ? (
+                    <form>
+                        <h2>PAGE 0 OF THE FORM</h2>
+                        <label for='question1'>Input between 1-7:</label><br></br>
+                        <input type='text' id='question1' name='question1'></input><br></br>
+                        <button onClick={nextPageFrom0}>nextPage</button>
+                    </form>
+                ) : (
+                    null
+                )
+            }
+
+            {
+                page1 === true ? (
+                    <form>
+                        <h2>PAGE 1 OF THE FORM</h2>
+                        <label for='question2'>Input between 1-7:</label><br></br>
+                        <input type='text' id='question2' name='question2'></input><br></br>
+                        <button onClick={prevPage}>prevPage</button>
+                        <button onClick={nextPageFrom1}>nextPage</button>
+                    </form>
+                ) : (
+                    null
+                )
+            }
+
+            {
+                page2 === true ? (
+                    <form>
+                        <p>page2 is true</p>
+
+                        <button onClick={prevPage}>prevPage</button>
+                        <button onClick={calculate}>calculate</button>
+                    </form>
+                ) : (
+                    null
+                )
+            }
+
         </div>
     )
 }
